6b0b2cd1b73a484e0976b291bb67b7c4573cfc9e
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2021-11-03 00:33:31 -0400

    finish API

--- HEARTbackend/settings.py
+++ HEARTbackend/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bvf33*jk87l=*3buf^u6fhbd@t(6e-uvjd=k=1^08ztli$agq6'
+SECRET_KEY = os.environ.get("PORT_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
